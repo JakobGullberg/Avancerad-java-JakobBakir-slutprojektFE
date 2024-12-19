e1650357693b23927245882f4c3bd716d218658d
Jakob Gullberg <Jakob.Gullberg@gritacademy.se>
2024-12-19 09:55:40 +0100

    Update helloController
    New ids

--- ToDoListFE/src/main/java/org/example/todolistfe/HelloController.java
+++ ToDoListFE/src/main/java/org/example/todolistfe/HelloController.java
@@ -1,14 +1,42 @@
 package org.example.todolistfe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField Input_Date;
+
+    @FXML
+    private TextField Input_Description;
+
+    @FXML
+    private TextField Input_TaskName;
+
+    @FXML
+    private TextArea Textarea_Allbox;
+
+    @FXML
+    void AddTask(ActionEvent event) {
+
     }
+
+    @FXML
+    void DeleteTask(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GetAllTasks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void UpdateTask(ActionEvent event) {
+
+    }
+
 }
